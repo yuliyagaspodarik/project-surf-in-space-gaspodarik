90896b7d4794d589d84f06197d4d0718fb8f211f
Yuliya Gaspodarik <gaspod.yu@gmail.com>
2020-06-14 01:38:41 +0300

    Add shots, explosions and score

--- js/game.js
+++ js/game.js
@@ -1,19 +1,32 @@
 var canvas = document.getElementById('game');
 var context = canvas.getContext('2d');
-var cometaSize = 30;
+var cometSize = 30;
 var spriteSize = 100;
+var shotSize = 50;
+
+context.font = '18px Arial';
+context.fillStyle = 'purple';
 
 var fon = new Image();
 fon.src = './img/fon.png';
 
-var cometaImg = new Image();
-cometaImg.src = './img/2.png';
+var comet = new Image();
+comet.src = './img/2.png';
 
 var spriteImg = new Image();
 spriteImg.src = './img/sprite.png';
 
+var shot = new Image();
+shot.src = './img/shot.png';
+
+var explosion = new Image();
+explosion.src = './img/explosion.png';
+
 var comets = [];
+var shots = [];
+var explosions = [];
 var timer = 0;
+var score = 0;
 var sprite = {
   size: spriteSize,
   mouseX: 200,
@@ -25,7 +38,7 @@ canvas.addEventListener('mousemove', (event) => {
   sprite.mouseY = event.offsetY;
 });
 
-fon.onload = function () {
+shot.onload = function () {
   game();
 };
 
@@ -37,22 +50,33 @@ function game() {
 
 function update() {
   timer++;
-  if (timer % 10 === 0) {
+  if (timer % 20 === 0) {
     comets.push({
-      size: cometaSize,
+      size: cometSize,
       posX: Math.floor(Math.random() * 400),
-      posY: -cometaSize,
+      posY: -cometSize,
       speedX: Math.floor(Math.random() * 4) - 1,
-      speedY: Math.floor(Math.random() * 4) + 1
+      speedY: Math.floor(Math.random() * 4) + 1,
+      flag: 0
     });
   }
   /*setInterval(() => {comets.push({
-      size: cometaSize,
+      size: cometSize,
       posX: Math.floor(Math.random() * 400),
-      posY: -cometaSize,
+      posY: -cometSize,
       speedX: Math.floor(Math.random() * 4) - 1,
       speedY: Math.floor(Math.random() * 4) + 1
     })}, 5000);*/
+
+  if (timer % 30 === 0) {
+    shots.push({size: shotSize, posX: sprite.mouseX - 20, posY: sprite.mouseY - 70, speedX: 0, speedY: -5})
+  }
+
+  for (var n = 0; n < shots.length; n++) {
+    shots[n].posY += shots[n].speedY;
+    if (shots[n].posY < -50) shots.splice(n, 1);
+  }
+
   for (var i = 0; i < comets.length; i++) {
     comets[i].posX += comets[i].speedX;
     comets[i].posY += comets[i].speedY;
@@ -62,15 +86,47 @@ function update() {
     if (comets[i].posX >= canvas.width - comets[i].size || comets[i].posX < 0) {
       comets[i].speedX = -comets[i].speedX;
     }
+
+    for (var j = 0; j < shots.length; j++) {
+      if (Math.abs(comets[i].posX - shots[j].posX) < cometSize && Math.abs(comets[i].posY - shots[j].posY) < cometSize) {
+        explosions.push({posX: comets[i].posX - comets[i].size / 2, posY: comets[i].posY - comets[i].size / 2, animX: 0, animY: 0});
+        comets[i].flag = 1;
+        score++;
+        shots.splice(j, 1);
+        break; //continue;
+      }
+    }
+    if (comets[i].flag === 1) {
+      comets.splice(i, 1);
+    }
   }
+   for (var m = 0; m < explosions.length; m++) {
+     explosions[m].animX += 0.1;
+     if (explosions[m].animX > 5) {
+       explosions[m].animY++;
+       explosions[m] = 0;
+     }
+     if (explosions[m].animY > 4) {
+       explosions.splice(m, 1);
+     }
+   }
+
+   //столкновение корабля с кометой - условие для cancelAF
 }
 
 function render() {
   context.drawImage(fon, 0, 0, canvas.width, canvas.height);
   context.drawImage(spriteImg, sprite.mouseX - spriteSize / 2, sprite.mouseY - spriteSize / 2, sprite.size, sprite.size);
   for (var i = 0; i < comets.length; i++) {
-    context.drawImage(cometaImg, comets[i].posX, comets[i].posY, comets[i].size, comets[i].size);
+    context.drawImage(comet, comets[i].posX, comets[i].posY, comets[i].size, comets[i].size);
   }
+  for (i = 0; i < shots.length; i++) {
+    context.drawImage(shot, shots[i].posX, shots[i].posY, shots[i].size, shots[i].size);
+  }
+  for (i = 0; i < explosions.length; i++) {
+    context.drawImage(explosion, 96 * Math.floor(explosions[i].animX), 96 * Math.floor(explosions[i].animY), 96, 96, explosions[i].posX, explosions[i].posY, 100, 100);
+  }
+  context.fillText('Score: ' + score, 20, 20);
 }
 
 var requestAnimateFrame =
